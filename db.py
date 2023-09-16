7bba0a0ccea5663768e3c2f576a561fce18bb678
NotyCode <josti-olaya@upc.edu.co>
2023-09-16 11:43:42 -0500

    Revert "priemr comit"
    This reverts commit fd7f3bce392deb0439914267ce05cfb8c63ed69f.

--- db.py
+++
@@ -1,7 +0,0 @@
-import databases
-import sqlalchemy
-from decouple import config
-
-DATABASE_URL= f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/web_indi_god"
-database = databases.Database(DATABASE_URL)
-metadata = sqlalchemy.MetaData()
